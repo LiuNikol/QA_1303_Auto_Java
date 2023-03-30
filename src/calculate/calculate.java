c0ff30ac4323a3f0fe4fe884464133e5e59940e7
LiuNikol <109183686+LiuNikol@users.noreply.github.com>
2023-03-30 18:08:19 +0300

    home_work_4_30032023_add simple Сalculator, add new two files(class)

---
+++ src/calculate/calculate.java
@@ -0,0 +1,20 @@
+package calculate;
+
+public class calculate {
+    public static int sum(int n, int m) {
+        return n+m;
+    }
+    public static int sub(int n,int m) {
+        return n-m;
+    }
+    public static int multiplication(int n, int m) {
+        return n*m;
+    }
+    public static int division(int n, int m) {
+        return n/m;
+    }
+    public static int percentage(int n, int m) {
+        return n*100/m;
+    }
+
+}
---
+++ src/calculate/homework4.java
@@ -0,0 +1,32 @@
+package calculate;
+
+import java.util.Scanner;
+public class homework4 {
+        public static void main(String[] args) {
+            Scanner scanner=new Scanner(System.in);
+            System.out.println("enter first number");
+            int number=scanner.nextInt(), number2,result = 0;
+            char simbol;
+            System.out.println("enter second number");
+            number2=scanner.nextInt();
+            System.out.println("enter symbol of operation");
+            simbol=scanner.next().charAt(0);
+            switch (simbol){
+                case '+': result=calculate.sum(number,number2);
+                    break;
+                case '-': result=calculate.sub(number,number2);
+                    break;
+                case '*': result=calculate.multiplication(number,number2);
+                    break;
+                case '/': result=calculate.division(number,number2);
+                    break;
+                case '%': result=calculate.percentage(number,number2);
+                    break;
+                default:
+                    System.out.println("Error");
+                    System.exit(0);
+                    break;
+            }
+            System.out.println(result);
+        }
+    }
