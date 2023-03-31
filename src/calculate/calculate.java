bc9597bc92e47f04614e724637bc5e17295503e3
LiuNikol <109183686+LiuNikol@users.noreply.github.com>
2023-03-31 15:23:34 +0300

    home_work_4_30032023_add check for zero in "/, %" and change int on double in "/, %"

--- src/calculate/calculate.java
+++ src/calculate/calculate.java
@@ -1,6 +1,7 @@
 package calculate;
 
 public class calculate {
+
     public static int sum(int n, int m) {
         return n+m;
     }
@@ -10,11 +11,16 @@ public class calculate {
     public static int multiplication(int n, int m) {
         return n*m;
     }
-    public static int division(int n, int m) {
-        return n/m;
+    public static double division(int n, int m) { //  double = numberrs after point= ex.=3/2=1.5
+        if(m != 0) { return ((double) n / (double) m); // if we have m=0 return result = 0
+        } else {
+            return 0;
+        }
     }
-    public static int percentage(int n, int m) {
-        return n*100/m;
+    public static double percentage(int n, int m) {
+        if(m != 0) { return ((double) n*100/(double) m);// if we have m=0 return result = 0
+        } else {
+            return 0;
+        }
     }
-
 }
--- src/calculate/homework4.java
+++ src/calculate/homework4.java
@@ -5,7 +5,9 @@ public class homework4 {
         public static void main(String[] args) {
             Scanner scanner=new Scanner(System.in);
             System.out.println("enter first number");
-            int number=scanner.nextInt(), number2,result = 0;
+            int number=scanner.nextInt();
+            int number2;
+            double result = 0;
             char simbol;
             System.out.println("enter second number");
             number2=scanner.nextInt();
@@ -18,8 +20,10 @@ public class homework4 {
                     break;
                 case '*': result=calculate.multiplication(number,number2);
                     break;
-                case '/': result=calculate.division(number,number2);
-                    break;
+                case '/':
+                    result=calculate.division(number,number2);
+
+                break;
                 case '%': result=calculate.percentage(number,number2);
                     break;
                 default:
