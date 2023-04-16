97c646618aacfcc7f74123858f098c8f51c8bd0a
LiuNikol <109183686+LiuNikol@users.noreply.github.com>
2023-04-17 00:13:20 +0300

    home_work_9_lesson_9_17_04_enum_Size

---
+++ src/home_work_9/HomeWork9.java
@@ -0,0 +1,9 @@
+package home_work_9;
+
+public class HomeWork9 {
+    public static void main(String[] args) {
+        for (Size size : Size.values()) {
+            System.out.println(size.toString());
+        }
+    }
+}
