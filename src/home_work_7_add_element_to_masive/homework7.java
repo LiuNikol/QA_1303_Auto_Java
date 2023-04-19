f9cfdaedeb529249c7ed6041e857dd432b4d3ad8
LiuNikol <109183686+LiuNikol@users.noreply.github.com>
2023-04-19 22:17:24 +0300

    home_work_7_lesson_5_19_04_масив_та_числа

--- src/home_work_7_add_element_to_masive/homework7.java
+++ src/home_work_7_add_element_to_masive/homework7.java
@@ -1,5 +1,6 @@
 package home_work_7_add_element_to_masive;
 
+
 import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
@@ -10,15 +11,19 @@ public class homework7 {
 
         while (true) { // додаємо безкінечний цикл, щоб можна було повторно запустити програму
 
-            /*create random massive (массив) --- У цьому прикладі ми створюємо об'єкт класу Random і використовуємо метод nextInt() для отримання рандомного числа. За допомогою циклу for ми заповнюємо масив array рандомними числами. Для виведення масиву на екран ми використовуємо метод toString() класу Arrays.*/
+            /*create random massive (массив) --- У цьому прикладі ми створюємо об'єкт класу
+            Random і використовуємо метод nextInt() для отримання рандомного
+            числа. За допомогою циклу for ми заповнюємо масив
+            array рандомними числами. Для виведення масиву на екран
+            ми використовуємо метод toString() класу Arrays.*/
             int size = 10;/*кількість показаних цифр */
             int upperBound = 100;/*які цифри можуть бути*/
             int[] array = new int[size];
-            Random random = new Random();
+            Random random = new Random();/*умова рандом*/
             for (int i = 0; i < size; i++) {
                 array[i] = random.nextInt(upperBound);
             }
-            System.out.println("Random array: " + Arrays.toString(array));/* нащ рандомний масив*/
+            System.out.println("Random array: " + Arrays.toString(array));/*наш створений масив*/
 
             /*rule:you can enter only numbers*/
             int value = 0;
@@ -40,18 +45,28 @@ public class homework7 {
                 System.exit(0);
             }
 
+
             /*show what we created from begin "create random massive (массив)"*/
             System.out.println("Show all numbers from massive: " + Arrays.toString(array));
 
-            /*rule: number of position must be only between size of massive*/
-            if (position < 0 || position >= size) {
-                array = arrays_details.arrayMister(array, value, -1); // update the array with new value at position 0
-                System.out.println("Changed first symbol to " + array[0] + ", All massive: " + Arrays.toString(array));
+            System.out.println("----------------------------------------------");
+
+            if (position == 0) {
+                // додавання елементу на початок масиву
+                int[] newArray = new int[array.length + 1];
+                newArray[0] = value;
+                System.arraycopy(array, 0, newArray, 1, array.length);
+                array = newArray;
+                System.out.println("Show our Special number before massive if our position = '0': " + Arrays.toString(array));
+                // додавання елементу на позицію в масиві
             } else {
                 int[] positionArray = {position};
                 array = arrays_details.arrayMister(array, value, positionArray); // update the array with new value at the given position
                 System.out.println("Show all numbers with our Special number to our Position: " + position + ",  our massive: " + Arrays.toString(array));
             }
+
+            System.out.println("----------------------------------------------");
+
 
             /* блок запиту на повторний запуск програми */
             System.out.println("Do you want to restart the program? (Y/N)");
